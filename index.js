d494af5072efe02460d2388f576ea7e40beb4d9f
Marwan Alsaydali <hey@marwanalsaydali.com>
2021-02-27 22:11:18 -0500

    preparing for the JavaScript

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ import "./src/styles/tabs.css";
 import "./src/styles/gallery.css";
 import tabs from "./src/scripts/tabs";
 import gallery from "./src/scripts/gallery";
+import soundBox from "./src/scripts/soundbox";
+
 tabs();
 gallery();
+soundBox();
